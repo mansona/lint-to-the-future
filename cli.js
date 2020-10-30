47e14a489987eb10a5777ada98d356be3253fa2d
Chris Manson <chris@manson.ie>
2020-10-30 23:26:28 +0000

    add support for eslint@7

--- cli.js
+++ cli.js
@@ -5,12 +5,6 @@ const { readFileSync, writeFileSync } = require('fs');
 
 const { CLIEngine } = eslint;
 
-if (!CLIEngine || !CLIEngine.version.startsWith(6)) {
-  throw new Error('This tool currently only supports the ESLint 6.x node api.');
-}
-
-console.log(CLIEngine.version);
-
 function ignoreError(error) {
   const ruleIds = error.messages.map(message => message.ruleId);
   const uniqueIds = [...new Set(ruleIds)];
@@ -18,8 +12,6 @@ function ignoreError(error) {
   const file = readFileSync(error.filePath, 'utf8');
 
   let firstLine = file.split('\n')[0];
-
-  console.log(firstLine);
 
   if (firstLine.includes('eslint-disable')) {
     console.warn('appending existing disables not supported yet');
@@ -29,10 +21,8 @@ function ignoreError(error) {
 }
 
 async function main() {
-  // 1. Create an instance.
   const cli = new CLIEngine();
 
-  // 2. Lint files.
   const report = cli.executeOnFiles(["."]);
 
   const errors = report.results.filter(result => result.errorCount > 0);
