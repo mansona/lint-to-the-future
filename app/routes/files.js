c87ac9b38198ec395554f87ecbcb9a1b0b22c18b
Chris Manson <chris@manson.ie>
2020-12-15 22:28:19 +0000

    add super basic "display files" modal

---
+++ app/routes/files.js
@@ -0,0 +1,17 @@
+import Route from '@ember/routing/route';
+
+export default class FilesRoute extends Route {
+  model(params) {
+    let application = this.modelFor('application');
+
+    let ruleValue = application[params.id];
+    let keys = Object.keys(ruleValue);
+
+    // the keys are ISO dates, so sorting them alphabetically always sorts them
+    // in date order too :tada:
+    let max = keys.sort((a, b) => a > b)[keys.length - 1];
+
+    // this just returns the most recent list of files
+    return ruleValue[max];
+  }
+}
