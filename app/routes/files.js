754c9300fac559f64eb6d096a1746108342906c3
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2021-04-12 17:18:45 +0200

    Show name of rule on the modal

--- app/routes/files.js
+++ app/routes/files.js
@@ -3,7 +3,6 @@ import Route from '@ember/routing/route';
 export default class FilesRoute extends Route {
   model(params) {
     let application = this.modelFor('application');
-
     let ruleValue = application[params.id];
     let keys = Object.keys(ruleValue);
 
@@ -12,6 +11,6 @@ export default class FilesRoute extends Route {
     let max = keys.sort((a, b) => a > b)[keys.length - 1];
 
     // this just returns the most recent list of files
-    return ruleValue[max];
+    return {fileList: ruleValue[max], rule: params.id};
   }
 }
