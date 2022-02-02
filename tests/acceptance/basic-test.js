daaf4f10765b84f2dc087905f3624c4915513bd5
Chris Manson <chris@manson.ie>
2022-02-02 16:12:21 +0000

    add test for basic functionality

---
+++ tests/acceptance/basic-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | basic', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('basic functionality', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-chart]').exists({count: 9})
+    // await this.pauseTest();
+
+    assert.dom('[data-test-chart="lint-to-the-future-ember-template:require-valid-alt-text"] [data-test-time-button="monthly"]').isChecked();
+    assert.dom('[data-test-chart="lint-to-the-future-eslint:ember/no-empty-glimmer-component-classes"] [data-test-time-button="daily"]').isChecked();
+    assert.dom('[data-test-chart="lint-to-the-future-eslint:prettier/prettier"] [data-test-time-button="weekly"]').isChecked();
+
+    await click('[data-test-view-files="lint-to-the-future-eslint:prettier/prettier"]');
+    assert.equal(currentURL(), '/files/lint-to-the-future-eslint:prettier%2Fprettier');
+    assert.dom('[data-test-file]').exists({count: 32})
+  });
+});
