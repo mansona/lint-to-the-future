01db3fcb7251cb4482787bff90f04a03652fb236
Michael Kerr <michael.kerr@mainmatter.com>
2024-10-02 17:30:31 +0100

    v4.4.1...v5.11.0

--- config/environment.js
+++ config/environment.js
@@ -2,19 +2,16 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'lint-to-the-future',
     environment,
     rootURL: '/',
     locationType: 'history',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false,
       },
     },
 
